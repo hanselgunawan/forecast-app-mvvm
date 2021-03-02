1b8febe83b5fd41ee8ffcfb62e7a50596907a2db
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2021-03-01 23:35:25 -0800

    added forecast layout

--- app/src/main/java/com/hanseltritama/forecastapp/ui/MainActivity.kt
+++ app/src/main/java/com/hanseltritama/forecastapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.hanseltritama.forecastapp
+package com.hanseltritama.forecastapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hanseltritama.forecastapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
